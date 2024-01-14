af9327a17a762410565f82eba35457450081fda8
LKrAxViCt0R <vickylakra10@gmail.com>
2024-01-14 13:15:42 +0530

    Add MoreQuestions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { MenuBar } from "./components/MenuBar/MenuBar";
+// import { MenuBar } from "./components/MenuBar/MenuBar";
+import { MoreQuestions } from "./components/MoreQuestions/MoreQuestions";
 import { TopBar } from "./components/TopBar/TopBar";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <MenuBar />
+      {/* <MenuBar /> */}
+      <MoreQuestions type="Linked" />
+      <MoreQuestions type="Related" />
     </div>
   );
 }
